91c968aae5ca4e8d6d157f8f667f25a64b45476e
okihara <okihara6@gmail.com>
2022-10-26 13:42:56 +0900

    paiza_b039

---
+++ app/Models/paiza_b039.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class paiza_b039 extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['x', 'y', 'a', 'b'];
+}
