b610591d662f399a843cfd31c5c1c02c06453365
okihara <okihara6@gmail.com>
2022-12-07 12:48:56 +0900

    C113.blade.php追加

---
+++ resources/views/C113.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>C113
+    </head>
+    <body >
+    
+    <form action="/C113" method="POST">
+    <input class="xxxxx" type="submit" value="Click Me!">
+    {{ csrf_field() }}
+</form>
+
+以下。。入力データ<br>
+<br>
+
+</body>
+</html>
