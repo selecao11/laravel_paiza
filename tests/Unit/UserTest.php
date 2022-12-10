e44b963b46ea673d77961e193e6b2ef19ea71f6a
okihara <okihara6@gmail.com>
2022-12-10 17:27:24 +0900

    C113_unset_data_head_result追加2

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -45,21 +45,41 @@ class UserTest extends TestCase
         $input_datas[6] = '6';
         $C113 = new C113_Controller();
         $headers = $C113->get_data_header($input_datas);
+        print_r("\n");
+        print_r("input_datas=");
+        print_r($input_datas);
+        print_r("\n");
         print_r("masu=".$headers['masu']."\n");
         print_r("saikoro=".$headers['saikoro']."\n");
         $this->assertTrue($headers['is_success'], true);
     }
 
-    public function test_C113_unset_head()
+    public function test_C113_unset_data_head()
+    /* 
+    #C113_unset_data_head_result.xmlファイル作成
+    type nul > ./C113_unset_data_head_result.xml
+
+    #UT実行
+    php artisan test --log-junit ./C113_unset_data_head_result.xml --filter test_C113_unset_data_head tests/Unit/UserTest.php
+    */
     {
-        $input_datas[0] = '4 2';
-        $input_datas[1] = '+';
-        $input_datas[2] = '1';
-        $input_datas[3] = '1';
+        $input_datas[0] = '5 3';
+        $input_datas[1] = 'x';
+        $input_datas[2] = '-';
+        $input_datas[3] = '+';
+        $input_datas[4] = '1';
+        $input_datas[5] = '1';
+        $input_datas[6] = '6';
         $C113 = new C113_Controller();
-        $unset_head = $C113->unset_head($input_datas);
-        print_r($unset_head['data']);
-        $this->assertTrue($unset_head['success'], true);
+        $c113_datas = $C113->unset_data_head($input_datas);
+        print_r("\n");
+        print_r("input_datas=");
+        print_r($input_datas);
+        print_r("\n");
+        print_r("c113_datas=");
+        print_r($c113_datas);
+        print_r("\n");
+        $this->assertTrue($c113_datas['is_success'], true);
     }
 
     public function test_C113_masu_judgment(){
