cfb63c3d3082a548c30286a91c467bc833e2099b
okihara <okihara6@gmail.com>
2022-12-12 20:49:55 +0900

    test25

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\C113_Controller;
 class UserTest extends TestCase
 {
     /**
-     * A basic unit test example.　 テスト2
+     * A basic unit test example.  テスト2
      *
      * @return void
      */
