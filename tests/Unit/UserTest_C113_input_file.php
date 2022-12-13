93b1cd969fc3c1b0faefb9a0f8def06ed679fab1
okihara <okihara6@gmail.com>
2022-12-13 14:30:06 +0900

    test_C113_input_file_終了

--- tests/Unit/UserTest_C113_input_file.php
+++ tests/Unit/UserTest_C113_input_file.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use App\Http\Controllers\C113_Controller;
 
-class UserTest extends TestCase
+class UserTest_C113_input_file extends TestCase
 {
     /**
      * A basic unit test example.  テスト2
@@ -15,18 +15,32 @@ class UserTest extends TestCase
     public function test_C113_input_file()
     /* 
     #C113_result.xmlファイル作成
-    type nul > ./C113_result.xml
+    type nul > ./C113_input_file_result.xml
 
     #UT実行
-    php artisan test --log-junit ./C113_result.xml --filter test_C113_input_file tests/Unit/UserTest.php
+    php artisan test --log-junit ./C113_input_file_result.xml --filter test_C113_input_file tests/Unit/UserTest_C113_input_file.php
     */    {
-        $file_name = "C:\\laravel_paiza\\app\\Http\\Controllers\\C113.txt";
+        $c113_file_name = "C:\\laravel_paiza\\app\\Http\\Controllers\\C113.txt";
+        $c113_file = file_get_contents($c113_file_name);
+        $input_datas[0] = '5 3';
+        $input_datas[1] = 'x';
+        $input_datas[2] = '-';
+        $input_datas[3] = '+';
+        $input_datas[4] = '1';
+        $input_datas[5] = '1';
+        $input_datas[6] = '6';
         $C113 = new C113_Controller();
-        $c113_datas = $C113->input_file($file_name);
-        print_r($c113_datas['data']);
-        $this->assertTrue($c113_datas['is_success'], true);
-        $c113_datas;
+        $c113_datas = $C113->input_file($c113_file_name);
+        print_r("\n");
+        print_r("ファイル生データ = ");
+        print_r($c113_file);
+        print_r("\n");
+        print_r("c113_datas = ");
+        print_r($c113_datas);
+        print_r("\n");
+        print_r("c113_datas_correct = ");
+        print_r($input_datas);
+        print_r("\n");
+        $this->assertEquals($c113_datas, $input_datas);
     }
-
-
 }
