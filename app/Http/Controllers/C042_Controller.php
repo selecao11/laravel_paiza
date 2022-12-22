c5fb0553e0c864ca4332a4c326d04a0e6d83c0ab
okihara <okihara6@gmail.com>
2022-12-22 13:35:09 +0900

    C042_Controller_normal

--- app/Http/Controllers/C042_Controller.php
+++ app/Http/Controllers/C042_Controller.php
@@ -5,112 +5,111 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Validator; // Validatorだけでも実行できる
 
-class C066_Controller extends Controller
+class C042_Controller extends Controller
 {
     /**
-    * 金魚すくい
+    * 成績表初期化
     *
     * @param strng  $goldfish_weights 金魚の重さ配列
     * @param strng  $headers ヘッダデータ配列
     * @return int   $success_goldfish ヘッダデータ配列
     * @todo         ポイがなくなるまで金魚すくいをする
     */
-    public function Scoop_Goldfish($headers,$goldfish_weights){
+    public function Aggregate_Grades_init($headers){
+        $tp=$headers['Total_participants'];
+        $ARRAY_INIT=0;
+        $GAME_COUNT=3;
+        for ($i=0;$i<$tp;++$i){
+            $Match_Result=array_fill($ARRAY_INIT, $GAME_COUNT, "-");
+            $Victory_Tables[$i]=$Match_Result;
+        }
+        return $Victory_Tables;
+    }
+    /**
+    * 成績表作成
+    *
+    * @param strng  $goldfish_weights 金魚の重さ配列
+    * @param strng  $headers ヘッダデータ配列
+    * @return int   $success_goldfish ヘッダデータ配列
+    * @todo         ポイがなくなるまで金魚すくいをする
+    */
+    public function Aggregate_Grades($headers,$Grades_Data){
         /**
-        * Scoop_Goldfish
+        * Aggregate_Grades
         *
-        * @var string   $masu_saikoro_counts
-        *               ヘッダ分割配列
-        * @var int      $gw_index
-        *               金魚の重さ配列のindex
-        * @var int      $goldfish_number
-        *               金魚の数
-        * @var int      $GOLD_NUMBER_LEN_SUB
-        *               金魚の重さ配列-1減算用定数 while　条件用
-        * @var int      $fish_net
-        *               網の数
         * @var int      $fish_net_durability
         *               網の耐久性
         */
         #定数
-        $GOLD_NUMBER_LEN_SUB_ONE= 1;
+        $ARRAY_INIT=0;
         #変数
-        $success_goldfish = 0;
-        $gw_index = 0;
-        $goldfish_number = $headers['goldfish_number'];
-        $fish_net = $headers['fish_net'];
-        $fish_net_durability = $headers['fish_net_durability'];
-        while($gw_index<=$goldfish_number - $GOLD_NUMBER_LEN_SUB_ONE){
-            if($fish_net<=0){
-                return $success_goldfish;
-            }
-            if ($fish_net_durability > $goldfish_weights[$gw_index]){
-                ++$success_goldfish;
-                #網の耐久がすくなくなる
-                $fish_net_durability = $fish_net_durability -
-                            $goldfish_weights[$gw_index];
-                ++$gw_index;
-            }else{
-                #網がぼろぼろ
-                --$fish_net;
-                $fish_net_durability =
-                    $headers['fish_net_durability'];#網の耐久性
-            }
+#        $victory_tables[i][j];
+        $Victory_Tables = $this->Aggregate_Grades_init($headers);
+        foreach($Grades_Data as $gd){
+            $Victory_Tables[$gd['f']-1][$gd['s']-1]='w';#勝者
+            $Victory_Tables[$gd['s']-1][$gd['f']-1]='L';#敗者
         }
-        return $success_goldfish;
+        return $Victory_Tables;
     }
 
     /**
-    * 全データから金魚の重さデータの取得
+    * 取得成績データのSORT
     *
-    * @param strng  $c066_datas         全データ配列
+    * @param strng  $c042_datas         全データ配列
     * @return strng $goldfish_weights   金魚の重さ配列
-    * @todo         読み込んだ全データ配列から金魚の重さだけを抜き取る
+    * @todo         抜き取った成績データをSORTする
     */
-    public function Goldfish_Data_split($c066_datas){
-        unset($c066_datas['0']);
+    public function Grades_Data_select_sort($Grades_Data){
+        foreach( $Grades_Data as $gdkey => $gd_row ) {
+            $tmp_f[$gdkey] = $gd_row["f"];
+            $tmp_s[$gdkey] = $gd_row["s"];
+          }
+            array_multisort( $tmp_f, SORT_ASC,
+                                        $tmp_s, SORT_ASC,
+                                        $Grades_Data);
+        return $Grades_Data;
+    }
+    /**
+    * 全データから成績データの取得
+    *
+    * @param strng  $c042_datas         全データ配列
+    * @return strng $goldfish_weights   金魚の重さ配列
+    * @todo         読み込んだ全データ配列から成績だけを抜き取る
+    */
+    public function Grades_Data_select($c042_datas){
+        unset($c042_datas['0']);
         #配列のindexを振り直し
-        $c066_datas = array_merge($c066_datas);
-        foreach($c066_datas as $c066_dv){
-            $goldfish_weights[] = intval($c066_dv);
+        $c042_datas = array_merge($c042_datas);
+        foreach($c042_datas as $c042_datas_i=> $c042_dv){
+            $w = explode(" ", $c042_dv);
+            $Grades_Data[$c042_datas_i]['f'] = intval($w[0]);
+            $Grades_Data[$c042_datas_i]['s'] = intval($w[1]);
+            #一時領域解放
+            unset($w);
         }
-        return $goldfish_weights;
+        return $Grades_Data;
     }
     /**
     * 全データからヘッダデータの取得
     *
-    * @param strng  $c066_datas 全データ配列
+    * @param strng  $c042_datas 全データ配列
     * @return strng $headers ヘッダデータ配列
     * @todo         読み込んだ全データ配列からヘッダデータだけを抜き取る
     */
-    public function HeadData_Split($c066_datas){
+    public function HeadData_Split($c042_datas){
         /**
         * HeadData_Split
         *
-        * @var          $masu_saikoro_counts
-        *               ヘッダ分割配列
-        * @var int      $GOLDFISH_NUMBER_ZERO
-        *               ヘッダ内の金魚の数の位置
-        * @var int      $FISH_NET_ONE
-        *               ヘッダ内のパイの数の位置
-        * @var int      $FISH_NET_DURABILITY
-        *               ヘッダ内のパイの耐久性
-        * @var int      $headers['goldfish_number']
-        *               金魚の数
-        * @var int      $headers['fish_net']
-        *               パイの数
-        * @var int      $headers['goldfish_weight']
-        *               金魚の重さ
+        * @var          $Participants_Numbers
+        *               大会参加者配列
+        * @var int      $headers['Total_participants']
+        *               大会の参加者の総数
         */
         #定数
-        $GOLDFISH_NUMBER_ZERO=0;
-        $FISH_NET_ONE=1;
-        $FISH_NET_DURABILITY = 2;
-        $masu_saikoro_count = explode(" ", $c066_datas[0]);
-        $headers['goldfish_number']=intval(
-            $masu_saikoro_count[$GOLDFISH_NUMBER_ZERO]);
-        $headers['fish_net']=intval($masu_saikoro_count[$FISH_NET_ONE]);
-        $headers['fish_net_durability']=intval($masu_saikoro_count[$FISH_NET_DURABILITY]);
+        $TOTAL_PARTCIPANTS_NUMBER_ZERO=0;
+        $Participants_Numbers = explode(" ", $c042_datas[0]);
+        $headers['Total_participants']=intval(
+            $Participants_Numbers[$TOTAL_PARTCIPANTS_NUMBER_ZERO]);
         return $headers;
     }
 
@@ -170,13 +169,12 @@ class C066_Controller extends Controller
     * @return strng $c066_datas     全データ配列
     * @todo 	 読み込んだファイルデータを配列にいれる
     */
-    public function input_file($c066_file_name){
-        //$file_name = "/var/www/html/laravel_app/app/Http/Controllers/C113.txt";
-        $c066_file = file_get_contents($c066_file_name);
-        //データファイルのファイル末尾改行の削除
-        $c066_file = trim($c066_file);
-        $c066_datas = explode("\r\n", $c066_file);
-        return $c066_datas;
+    public function input_file($c042_file_name){
+        $c042_file = file_get_contents($c042_file_name);
+        //一行になっている入力データのファイル末尾改行の削除
+        $c042_file = trim($c042_file);
+        $c042_datas = explode("\n", $c042_file);
+        return $c042_datas;
     }
 
     public function output_C042($file_name){
@@ -192,13 +190,12 @@ class C066_Controller extends Controller
         }
         //入力データからヘッダーを削除
 #        $$c066_datas = $this-> unset_data_head($c066_datas);
-        //データファイルから成績データを抽出してSORTする。
+        //データファイルから成績データを抽出する。
         $Gradebooks = $this->Grades_Data_selec($c042_datas);
-        $Gradebooks = $this->Grades_Data_select_sort($Gradebooks);
 
         //リーグ表の作成開始
-        $success_goldfish = $this->Aggregate_Grades($headers,$goldfish_weights);
-
+        $success_goldfish = $this->Aggregate_Grades($headers,$Gradebooks);
+        $Gradebooks = $this->Grades_Data_select_sort($Gradebooks);
         //リーグ表の作成結果整理
         $C042['goldfish_number']=$success_goldfish;
 
