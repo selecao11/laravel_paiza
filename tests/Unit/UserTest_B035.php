cf1e38d3e4e762273019fea0ba575a6cd4355455
selecao11 <okihara.6k@gmail.com>
2023-01-30 13:43:53 +0900

    B035_test作成中6

--- tests/Unit/UserTest_B035.php
+++ tests/Unit/UserTest_B035.php
@@ -274,5 +274,81 @@ class UserTest_B035 extends TestCase
         $this->assertEquals($success_callLastmonth, $expected_value);
     }
 
+    public function testB035Grades(){
+        /* 
+        #B035judGrades_test_result.xmlファイル作成
+            type nul > /home/user/docker-laravel/laravel_paiza/tests/Unit/B035judGrades_test_result.xml
+            echo "" > /home/user/docker-laravel/laravel_paiza/tests/Unit/B035judGrades_test_result.xml
+        #UT実行
+        php artisan test    --log-junit /home/user/docker-laravel/laravel_paiza/tests/Unit/B035judGrades_test_result.xml --filter testB035Grades tests/Unit/UserTest_B035.php
+        */
+        $B035 = new B035_Controller();
+        #期待値取得
+        $expected_value['alice'] = [7,'UP!'];
+        $expected_value['betty'] = [4,'UP!'];
+        $expected_value['ngng'] = [4,'New'];
+        $expected_value['franca'] = [3,'Down!'];
+        $expected_value['dad'] = [2,'Down!'];
+
+        $cumulative_distances['alice'] = [7,''];
+        $cumulative_distances['betty'] = [4,''];
+        $cumulative_distances['ngng'] = [4,'New'];
+        $cumulative_distances['franca'] = [3,''];
+        $cumulative_distances['dad'] = [2,''];
+ 
+        $last_months_joggings['alice'] = 9;
+        $last_months_joggings['betty'] = 6;
+        $last_months_joggings['franca'] = 2;
+        $last_months_joggings['dad'] = 1;
+
+        $success_callLastmonth = $B035->callGrades($cumulative_distances,$last_months_joggings);
+
+        print_r("\n");
+        print_r("期待値 = ");
+        print_r("\n");
+        print_r($expected_value);
+        print_r("処理結果 = ");
+        print_r($success_callLastmonth);
+        $this->assertEquals($success_callLastmonth, $expected_value);
+    }
+
+    public function testB035Gradebook(){
+        /* 
+        #B035createGradebook_test_result.xmlファイル作成
+            type nul > /home/user/docker-laravel/laravel_paiza/tests/Unit/B035createGradebook_test_result.xml
+            echo "" > /home/user/docker-laravel/laravel_paiza/tests/Unit/B035createGradebook_test_result.xml
+        #UT実行
+        php artisan test    --log-junit /home/user/docker-laravel/laravel_paiza/tests/Unit/B035createGradebook_test_result.xml --filter testB035Gradebook tests/Unit/UserTest_B035.php
+        */
+        $B035 = new B035_Controller();
+        #期待値取得
+        $expected_value=array(
+            array('alice',7,'UP!'),
+            array('betty',4,'UP!'),
+            array('ngng',4,'New'),
+            array('franca',3,'Down!')
+        );
+
+        $fixed_value_read['N'] = 6;
+        $fixed_value_read['M'] = 7;
+        $fixed_value_read['T'] = 4;
+
+        $cumulative_distances['alice'] = [7,'UP!'];
+        $cumulative_distances['betty'] = [4,'UP!'];
+        $cumulative_distances['ngng'] = [4,'New'];
+        $cumulative_distances['franca'] = [3,'Down!'];
+        $cumulative_distances['dad'] = [2,'Down!'];
+
+
+        $success_callLastmonth = $B035->callGradebook($cumulative_distances,$fixed_value_read);
+
+        print_r("\n");
+        print_r("期待値 = ");
+        print_r("\n");
+        print_r($expected_value);
+        print_r("処理結果 = ");
+        print_r($success_callLastmonth);
+        $this->assertEquals($success_callLastmonth, $expected_value);
+    }
 
 }
